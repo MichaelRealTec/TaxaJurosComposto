68fec94c05d2cadb9bc57e4cbbfdbd7bd19dce87
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-25 19:45:06 -0300

    implementando a classe programa princial

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,15 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
 		
+		BrazilInterestService is = new BrazilInterestService(2.0);
+		double payment = is.payment(amount, months);
+		
+		System.out.println("Payment after " + months + "months:");
+		System.out.println(String.format("%.2f", payment));
 		
 		sc.close();
 	}
