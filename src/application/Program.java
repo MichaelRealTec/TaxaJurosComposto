5db3ae8462b6de63374a96eb5f3dfb9cdabc8c38
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-25 19:40:38 -0300

    primeiro commit arrumando

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,8 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
+		
+		System.out.println("Amount: ");
 		
 		
 		sc.close();
