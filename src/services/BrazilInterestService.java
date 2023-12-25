46d37185c51f95d714424825d6930df16911d34a
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-25 20:11:17 -0300

    alterando método para default para aproveitar o reuso

--- src/services/BrazilInterestService.java
+++ src/services/BrazilInterestService.java
@@ -1,7 +1,5 @@
 package services;
 
-import java.security.InvalidParameterException;
-
 public class BrazilInterestService implements InterestService{
 	// Atributos
 	private double interestRate;
@@ -20,11 +18,5 @@ public class BrazilInterestService implements InterestService{
 	}
 		
 	// Métodos
-	@Override
-	public double payment(double amount, int months) {
-		if(months < 1) {
-			throw new InvalidParameterException("Months must be greater than zero");
-		}
-		return amount * Math.pow(1.0 + interestRate / 100.0, months);
-	}
+
 }
